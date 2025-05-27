07cee6696f52043e0c7b1ec512d420b984969558
Rouf1224 <ahmadroufr24@gmail.com>
2025-05-27 15:58:33 +0700

    membuat data agregasi

---
+++ agregasi.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+#include "anak.hpp"
+#include "ibu.hpp"
+
+int main() {
+    ibu* varibu = new ibu("dini");
+    ibu* varibu2 = new ibu("novi");
+    anak* varAnak1 = new anak("tono");
+    anak* varAnak2 = new anak("rini");
+    anak* varAnak3 = new anak("dewi");
+
+    varibu->tambahAnak(varAnak1);
+    varibu->tambahAnak(varAnak2);
+    varibu2->tambahAnak(varAnak3);
+    varibu2->tambahAnak(varAnak1);
+    
+    
+
+}
